d57de5bdbc4fca9f4a35d4d314eb6605d7fa579b
lorddusk <lang2619@gmail.com>
2021-12-02 14:54:53 +0100

    Day02_Dive!

---
+++ Day02_Dive/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Utils;
+
+namespace Day02_Dive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] stringInput = Read.Input("day02");
+
+            (int horizontal, int depth) = SimpleMove(stringInput);
+            Console.WriteLine($"Part One: {horizontal * depth} horizontal * depth.");
+            (horizontal, depth) = AdvancedMove(stringInput);
+            Console.WriteLine($"Part Two: {horizontal * depth} horizontal * depth.");
+        }
+
+        public static (int, int) SimpleMove(string[] input)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            foreach (string line in input)
+            {
+                string[] splittedLine = line.Split(" ");
+                switch (splittedLine[0])
+                {
+                    case "forward":
+                        horizontal += int.Parse(splittedLine[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(splittedLine[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(splittedLine[1]);
+                        break;
+                }
+            }
+
+            return (horizontal, depth);
+        }
+
+        public static (int, int) AdvancedMove(string[] input)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+            
+            foreach (string line in input)
+            {
+                string[] splittedLine = line.Split(" ");
+                switch (splittedLine[0])
+                {
+                    case "forward":
+                        horizontal += int.Parse(splittedLine[1]);
+                        depth += aim * int.Parse(splittedLine[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(splittedLine[1]);
+                        break;
+                    case "down":
+                        aim += int.Parse(splittedLine[1]);
+                        break;
+                }
+            }
+
+            return (horizontal, depth);
+        }
+    }
+}
---
+++ Day02_Dive/Tests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utils;
+
+namespace Day02_Dive
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void SimpleMove_Example_Success()
+        {
+            // Arrange
+            string[] stringInput = new string[] {"forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2"};
+
+            // Act
+            (int horizontal, int depth) = Program.SimpleMove(stringInput);
+
+            // Assert
+            int expectedOutput = 150;
+            expectedOutput.Should().Be(horizontal*depth);
+        }
+
+        [TestMethod]
+        public void SimpleMove_Exercise_Success()
+        {
+            // Arrange
+            string[] stringInput = Read.Input("day02");
+            
+            // Act
+            (int horizontal, int depth) = Program.SimpleMove(stringInput);
+
+            // Assert
+            int expectedOutput = 1383564;
+            expectedOutput.Should().Be(horizontal*depth);
+        }
+        
+        [TestMethod]
+        public void AdvancedMove_Example_Success()
+        {
+            // Arrange
+            string[] stringInput = new string[] {"forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2"};
+
+            // Act
+            (int horizontal, int depth) = Program.AdvancedMove(stringInput);
+
+            // Assert
+            int expectedOutput = 900;
+            expectedOutput.Should().Be(horizontal*depth);
+        }
+        
+        [TestMethod]
+        public void AdvancedScan_Exercise_Success()
+        {
+            // Arrange
+            string[] stringInput = Read.Input("day02");
+            
+            // Act
+            (int horizontal, int depth) = Program.AdvancedMove(stringInput);
+
+            // Assert
+            int expectedOutput = 1488311643;
+            expectedOutput.Should().Be(horizontal*depth);
+        }
+    }
+}
