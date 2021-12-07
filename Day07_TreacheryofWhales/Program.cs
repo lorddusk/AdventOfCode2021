40c556152d59dd1de2c9cb11c8ee3e409818d8bf
lorddusk <lang2619@gmail.com>
2021-12-07 12:00:18 +0100

    Day 07 - Completed

---
+++ Day07_TreacheryofWhales/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Xml.Linq;
+using StringUtilities;
+
+namespace Day07_TreacheryofWhales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringInput = Utils.Input("day07");
+            int[] input = Utils.StringToIntArray(stringInput[0].Split(","));
+
+            int outcome = 0;
+            outcome = SimpleProgram(input);
+            Console.WriteLine($"Part One: {outcome}.");
+            AdvancedProgram(input);
+            Console.WriteLine($"Part Two: {outcome}.");
+        }
+
+        public static int SimpleProgram(int[] input)
+        {
+            Array.Sort(input);
+            int first = input[0];
+            int last = input[input.Length-1];
+            Dictionary<int, int> positionFuel = new Dictionary<int, int>();
+
+            for (int pos = first; pos < last; pos++)
+            {
+                foreach (int crab in input)
+                {
+                    int cost = crab - pos;
+                    cost = Math.Abs(cost);
+                    DictionaryFuelCost(positionFuel, pos, cost);
+                }
+            }
+
+            var list = positionFuel.ToList();
+            list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            return list[0].Value;
+        }
+
+        public static int AdvancedProgram(int[] input)
+        {
+            Array.Sort(input);
+            int first = input[0];
+            int last = input[input.Length-1];
+            Dictionary<int, int> positionFuel = new Dictionary<int, int>();
+
+            for (int pos = first; pos < last; pos++)
+            {
+                foreach (int crab in input)
+                {
+                    int fuelCost = 0;
+                    int difference = Math.Abs(crab - pos);
+                    for (int i = 1; i <= difference; i++)
+                    {
+                        fuelCost += (1 * i);
+                    }
+                    DictionaryFuelCost(positionFuel, pos, fuelCost);
+                }
+            }
+
+            var list = positionFuel.ToList();
+            list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            return list[0].Value;
+        }
+        
+        public static void DictionaryFuelCost(Dictionary<int, int> dictionary, int position, int fuelCost)
+        {
+            if (dictionary.ContainsKey(position))
+            {
+                dictionary[position] += fuelCost;
+            }
+            else
+            {
+                dictionary.Add(position, fuelCost);
+            }
+        }
+    }
+}
---
+++ Day07_TreacheryofWhales/Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilities;
+
+namespace Day07_TreacheryofWhales
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void SimpleScan_Example_Success()
+        {
+            // Arrange
+            int[] input = new int[] {16,1,2,0,4,2,7,1,2,14};
+
+            // Act
+            int actualOutput = Program.SimpleProgram(input);
+
+            // Assert
+            int expectedOutput = 37;
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        [TestMethod]
+        public void SimpleScan_Exercise_Success()
+        {
+            // Arrange
+            string[] strInput = Utils.Input("day07");
+            int[] input = Utils.StringToIntArray(strInput[0].Split(","));
+
+            // Act
+            int actualOutput = Program.SimpleProgram(input);
+
+            // Assert
+            int expectedOutput = 352254;
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        [TestMethod]
+        public void AdvancedScan_Example_Success()
+        {
+            // Arrange
+            int[] input = new int[] {16,1,2,0,4,2,7,1,2,14};
+
+            // Act
+            int actualOutput = Program.AdvancedProgram(input);
+
+            // Assert
+            int expectedOutput = 168;
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        [TestMethod]
+        public void AdvancedScan_Exercise_Success()
+        {
+            // Arrange
+            string[] strInput = Utils.Input("day07");
+            int[] input = Utils.StringToIntArray(strInput[0].Split(","));
+
+            // Act
+            int actualOutput = Program.AdvancedProgram(input);
+
+            // Assert
+            int expectedOutput = 99053143;
+            actualOutput.Should().Be(expectedOutput);
+        }
+    }
+}
