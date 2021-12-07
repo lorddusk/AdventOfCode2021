bf1fcec4a5d8f9f4184b39d15675f868083ec265
lorddusk <lang2619@gmail.com>
2021-12-07 11:33:13 +0100

    Day 5 - Project File - Not started

---
+++ Day05_HydrothermalVenture/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using StringUtilities;
+
+namespace Day05_HydrothermalVenture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringInput = Utils.Input("day05");
+
+            int outcome = 0;
+            SimpleProgram(stringInput);
+            Console.WriteLine($"Part One: {outcome}.");
+            AdvancedProgram(stringInput);
+            Console.WriteLine($"Part Two: {outcome}.");
+        }
+
+        public static void SimpleProgram(string[] input)
+        {
+        }
+
+        public static void AdvancedProgram(string[] input)
+        {
+        }
+    }
+}
---
+++ Day05_HydrothermalVenture/Tests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilities;
+
+namespace Day05_HydrothermalVenture
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void SimpleScan_Example_Success()
+        {
+            // Arrange
+            string[] input = new string[]
+            {
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2"
+            };
+
+            // Act
+            Program.SimpleProgram(input);
+
+            // Assert
+            int actualOutput = 0;
+            int expectedOutput = 0;
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        [TestMethod]
+        public void SimpleScan_Exercise_Success()
+        {
+            // Arrange
+            string[] input = Utils.Input("day05");
+
+            // Act
+            Program.SimpleProgram(input);
+
+            // Assert
+            int actualOutput = 0;
+            int expectedOutput = 0;
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        [TestMethod]
+        public void AdvancedScan_Example_Success()
+        {
+            // Arrange
+            string[] input = new string[]
+            {
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2"
+            };
+
+            // Act
+            Program.AdvancedProgram(input);
+
+            // Assert
+            int actualOutput = 0;
+            int expectedOutput = 0;
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        [TestMethod]
+        public void AdvancedScan_Exercise_Success()
+        {
+            // Arrange
+            string[] input = Utils.Input("day05");
+
+            // Act
+            Program.AdvancedProgram(input);
+
+            // Assert
+            int actualOutput = 0;
+            int expectedOutput = 0;
+            actualOutput.Should().Be(expectedOutput);
+        }
+    }
+}
